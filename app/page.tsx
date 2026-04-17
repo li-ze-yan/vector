51c74069c55348ba85de3fdedf06201076bd8a82
tkx.cool <kunxin.tan@fanyou-online.com>
2026-04-17 14:18:58 +0800

    feat: change app home page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { ThemeToggle } from "@/components";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-white/50 dark:bg-black/50 flex flex-col justify-center items-center gap-4">
+    <div className="w-screen h-screen bg-white/50 dark:bg-black/50 flex flex-col justify-center items-center gap-4 font-sans">
+      <h1 className="text-5xl font-extrabold italic">Vector Lab</h1>
       <p className="text-2xl font-sans">Inter (font-sans)</p>
       <p className="text-2xl font-source">Source Sans Pro (font-source)</p>
       <p className="text-2xl font-mono">IBM Plex Mono (font-mono)</p>
@@ -11,9 +11,6 @@ export default function Home() {
         Ubuntu Mono
       </p>
       <ThemeToggle />
-      <Link href="/docs/example" className="text-blue-500 underline hover:text-blue-700">
-        Docs Example
-      </Link>
     </div>
   );
 }
