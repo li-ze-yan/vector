5a02be66f62d300d197b1aec56fadc9353e056ca
tkx.cool <kunxin.tan@fanyou-online.com>
2026-04-18 20:16:33 +0800

    feat: set large container to show

--- app/(docs)/docker-docs/[slug]/page.tsx
+++ app/(docs)/docker-docs/[slug]/page.tsx
@@ -41,7 +41,7 @@ export default async function DocPage(props: DocsProps) {
     <>
       {/* Add a placeholder div so the Next.js router can find the scrollable element. */}
       <div hidden />
-      <div className="mx-auto grid w-full max-w-2xl grid-cols-1 gap-10 xl:max-w-5xl xl:grid-cols-[minmax(0,1fr)_var(--container-2xs)]">
+      <div className="mx-auto grid w-full max-w-2xl grid-cols-1 gap-10 xl:max-w-7xl xl:grid-cols-[minmax(0,1fr)_var(--container-2xs)]">
         <div className="px-4 pt-10 pb-24">
           <h1
             data-title="true"
